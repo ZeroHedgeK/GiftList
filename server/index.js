0a721e6d4f61164d1de2d3a659b2f4462cfc97bf
ZeroHedgeK <83179771+ZeroHedgeK@users.noreply.github.com>
2023-07-31 12:09:04 +0300

    AU week 2 Merkle Tree Gift List Project done!

--- server/index.js
+++ server/index.js
@@ -8,14 +8,14 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '3816dd482b45e769bc608c073750b56677eefe7bbe1176a79cd431d69e9bdef0';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
